a8fcf678690069bdff8a2df7c0904ec2bdb45d60
vanak <ven2kaze@gmail.com>
2020-08-07 13:14:22 +0200

    added cartItem to cartDropdown

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import CustomButton from '../custom-button/CustomButton';
+import CartItem from '../cart-item/CartItem';
+
 import './CartDropdown.styles.scss';
 
-const CartDropdown = () => {
+const CartDropdown = ({ items }) => {
     return (
         <div className='cart-dropdown'>
-            <div className='cart-items' />
+            <div className='cart-items'>
+            { items.map(item => (<CartItem key={item.id} item={item} />))}
+            </div>
             <CustomButton>GO TO CHECKOUT</CustomButton>
         </div>
     );
 };
 
-export default CartDropdown;
+const mapStateToProps = state => ({
+    items: state.cart.cartItems
+});
+export default connect(mapStateToProps)(CartDropdown);
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -9,22 +9,13 @@ const firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-// How to store data from auth inside cloud firestore
-// 1. Create function that have 2 parameters, the userAuth: object and additonalData: object
 export const createUserProfileDocument = async (userAuth, additionalData) => {
-  // 2. Check if userAuth exists, if not return this function
   if (!userAuth) return;
-  // 3. Create a document reference by querying firestore
-  // To get back the user reference at that location
   const userRef = firestore.doc(`users/${userAuth.uid}`);
-  // 4. Get back the document snapShot === data at this current place
   const snapShot = await userRef.get();
-  // 5. Using that snapshot to check whether or not there's data there 
-  // if it doesn't exist we want to create a piece of data to store inside database 
   if (!snapShot.exists) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
-    // 6. Set database with the new object
     try {
       await userRef.set({
         displayName,
@@ -36,7 +27,6 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
       console.log('error creating user', error.message);
     };
   }
-  // 7. Return userRef to use after 
   return userRef;
 };
 
